8777640364a2199c428115fb6cc306901dedb9ca
Shivang-Namdeo-12 <145819416+Shivang-Namdeo-12@users.noreply.github.com>
2026-03-19 12:31:28 +0530

    Update countoccurance.java

---
+++ countoccurance.java
@@ -0,0 +1,24 @@
+public class countoccurance {
+  public int[] occurrencesOfElement(int[] nums, int[] queries, int x) {
+    int[] ans = new int[queries.length];
+
+    for (int i = 0; i < queries.length; i++) {
+      int count = 0;
+      int index = -1;
+
+      for (int j = 0; j < nums.length; j++) {
+        if (nums[j] == x) {
+          count++;
+          if (count == queries[i]) {
+            index = j;
+            break;
+          }
+        }
+      }
+
+      ans[i] = index;
+    }
+
+    return ans;
+  }
+}

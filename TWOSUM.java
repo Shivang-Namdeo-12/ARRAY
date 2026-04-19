3b0a5d917f5ac96353ebfb1f6889b20010f74811
Shivang-Namdeo-12 <145819416+Shivang-Namdeo-12@users.noreply.github.com>
2026-04-19 11:37:51 +0530

    Update TWOSUM.java

--- TWOSUM.java
+++ TWOSUM.java
@@ -15,6 +15,8 @@ public class TWOSUM {
         return ar; // default (if not found)
     }
 
+    
+
     public static void main(String[] args) {
         int nums[] = {2, 7, 11, 15};
         int target = 9;
